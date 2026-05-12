cabd4b0c8afe263e2e4dada6fd85bddf3f0dd01c
fatecprojetoextensionista <fatecprojetoextensionista@gmail.com>
2026-05-12 18:14:27 -0300

    Update Article.tsx

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,3 +1,4 @@
+import ReactMarkdown from 'react-markdown';
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { supabase } from "@/integrations/supabase/client";
