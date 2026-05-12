a45b31a31aac17037159e6aecbd2e2c86bcdec50
fatecprojetoextensionista <fatecprojetoextensionista@gmail.com>
2026-05-12 16:39:45 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -215,7 +215,6 @@ export default function Home() {
           {/* SIDEBAR */}
           <div className="space-y-6">
             <TrendingWidget items={trending} />
-           //<NewsletterWidget />
             <CategoriesWidget categories={categories} />
           </div>
         </div>
